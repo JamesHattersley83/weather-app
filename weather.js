f389619bcd4aea8a8c8a599e2bcbf5520f7a3cb4
James Hattersley <jameshattersley83@gmail.com>
2019-05-03 20:41:37 +0100

    create weather class and fetch weather data

---
+++ weather.js
@@ -0,0 +1,27 @@
+// create weather class
+class Weather {
+  constructor(city, country) {
+    this.apiKey = "726003673e20d47d73ff21749e26e0e9";
+    this.city = city;
+    this.country = country;
+  }
+
+  // fetch weather from API
+  async getWeather() {
+    const response = await fetch(
+      `api.openweathermap.org/data/2.5/weather?q=${this.city},${
+        this.country
+      }&APPID=${this.apiKey}`
+    );
+
+    const responseData = await response.json;
+
+    return responseData;
+  }
+
+  // change weather location
+  changeLocation(city, country) {
+    this.city = city;
+    this.country = country;
+  }
+}
