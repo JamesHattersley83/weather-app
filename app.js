8ce91ccce9358d8702e334565ef3a50e7a0cafc5
James Hattersley <jameshattersley83@gmail.com>
2019-05-04 21:30:39 +0100

    saved weather data to local stoage

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+// initialise storage class
+const storage = new Storage();
+
+// get location from stored data
+const weatherLocation = storage.getLocationData();
+
 // initialise weather obj
-const weather = new Weather("Leeds", "uk");
+const weather = new Weather(weatherLocation.city, weatherLocation.country);
 
 // initialise UI obj
 const ui = new UI();
@@ -12,7 +18,11 @@ document.getElementById("change-btn").addEventListener("click", e => {
   const city = document.getElementById("city").value;
   const country = document.getElementById("country").value;
 
+  // Change location
   weather.changeLocation(city, country);
+
+  // set location to local storage
+  storage.setLocationData(city, country);
 
   // get weather and display UI again
   getWeather();
